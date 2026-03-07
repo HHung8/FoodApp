e4e71e080980ba1ae78df757c324d09217073400
huuhung <nghuuhung08102002@gmal.com>
2026-03-07 09:47:46 +0700

    first commit

---
+++ src/auth/Login.tsx
@@ -0,0 +1,94 @@
+import { Loader2, LockKeyhole, Mail } from "lucide-react";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Separator } from "../components/ui/separator";
+import { Link } from "react-router-dom";
+import { useState, type ChangeEvent } from "react";
+
+interface LoginInputState {
+  email: string;
+  password: string;
+}
+
+const Login = () => {
+  const [input, setInput] = useState<LoginInputState>({
+    email: "",
+    password: "",
+  });
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const loginSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  const loading = false;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 rounded-lg"
+      >
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">Yushing Dev Eats</h1>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="pl-10 focus-visible:ring-1"
+              value={input.email}
+              onChange={changeEventHandler}
+            />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="pl-10 focus-visible:ring-1"
+              value={input.password}
+              onChange={changeEventHandler}
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-10">
+          {loading ? (
+            <Button
+              type="submit"
+              className="w-full bg-orange hover:bg-hoverOrange"
+            >
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="w-full bg-orange hover:bg-hoverOrange"
+            >
+              Login
+            </Button>
+          )}
+        </div>
+        <Separator />
+        <p className="mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500">
+            Signup
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/auth/Signup.tsx
@@ -0,0 +1,115 @@
+import { useState, type ChangeEvent, type FormEvent } from "react"
+import { Input } from "../components/ui/input";
+import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
+import { Button } from "../components/ui/button";
+import { Separator } from "../components/ui/separator";
+import { Link } from "react-router-dom";
+
+interface SignupInputState {
+    fullname: string,
+    email: string,
+    password: string,
+    contact: string
+}
+
+const Signup = () => {
+    const [input, setInput] = useState({
+        fullname:"",
+        email:"",
+        password:"",
+        contact:"",
+    });
+    const changeEventHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setInput({...input, [name]:value});
+    }
+    const signupSubmitHandler = async(e:FormEvent) => {
+        e.preventDefault();
+        console.log(input);
+    }
+    const loading = false;
+    return (
+    <div className="flex items-center justify-center min-h-screen">
+        <form 
+          onSubmit={signupSubmitHandler}
+          className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
+        >
+            <div className="mb-4">
+                <h1 className="font-bold text-2xl">Yushing Dev Eats</h1>
+            </div>
+            <div className="mb-4">
+                <div className="relative">
+                    <Input 
+                        type="text"
+                        placeholder="Full Name"
+                        name="fullname"
+                        value={input.fullname}
+                        onChange={changeEventHandler}
+                        className="pl-10 focus-visible:ring-1"
+                    />
+                    <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="relative">
+                    <Input 
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={input.email}
+                        onChange={changeEventHandler}
+                        className="pl-10 focus-visible:ring-1"
+                    />
+                    <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="relative">
+                    <Input 
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={input.password}
+                        onChange={changeEventHandler}
+                        className="pl-10 focus-visible:ring-1"
+                    />
+                    <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                </div>
+            </div>
+            <div className="mb-4">
+                <div className="relative">
+                    <Input 
+                        type="text"
+                        placeholder="Contact"
+                        name="contact"
+                        value={input.contact}
+                        onChange={changeEventHandler}
+                        className="pl-10 focus-visible:ring-1"
+                    />
+                    <PhoneOutgoing className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                </div>
+            </div>
+            <div className="mb-10">
+                {
+                    loading ? (
+                        <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                        </Button>
+                    ): (
+                        <Button type="submit" className="w-full bg-orange hover:bg-hoverOrange"> 
+                            Signup
+                        </Button>
+                    )
+                }
+            </div>
+            <Separator />
+            <p className="mt-2">
+                Already have an account? {" "}
+                <Link to="/login" className="text-blue-500">Login</Link>
+            </p>
+        </form>
+    </div>
+  )
+}
+
+export default Signup
